5f9846485c12b1b12bc6910a5a40e99357bed6f7
Kia Lam <digitalanime@gmail.com>
2019-04-25 16:33:47 -0400

    Create shared tooltip component for charts to use.

--- src/SmartComponents/SamplePage/BarChart.js
+++ src/SmartComponents/SamplePage/BarChart.js
@@ -1,242 +1,192 @@
 /* eslint-disable */
 import React, { Component } from 'react';
 import * as d3 from 'd3';
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
+import Tooltip from '../../Utilities/Tooltip';
 
 class BarChart extends Component {
+  constructor(props) {
+    super(props);
+    this.margin = { top: 20, right: 20, bottom: 50, left: 70 };
+    this.init = this.init.bind(this);
+    this.resize = this.resize.bind(this);
+    this.state = {
+      data: []
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.server = process.env.REACT_APP_SERVER_ADDRESS ? process.env.REACT_APP_SERVER_ADDRESS : window.location.host;
-        this.protocol = process.env.REACT_APP_SERVER_PROTOCOL ? process.env.REACT_APP_SERVER_PROTOCOL : 'https';
+  // Methods
+  resize(fn, time) {
+    let timeout;
+
+    return function() {
+      const functionCall = () => fn.apply(this, arguments);
+
+      clearTimeout(timeout);
+      timeout = setTimeout(functionCall, time);
+    };
+  }
+
+  async init() {
+    // Clear our chart container element first
+    d3.selectAll('#d3-chart-root > *').remove();
+    const width =
+        parseInt(d3.select('#' + this.props.id).style('width')) -
+        this.margin.left -
+        this.margin.right,
+      height =
+        parseInt(d3.select('#' + this.props.id).style('height')) -
+        this.margin.top -
+        this.margin.bottom;
+    const parseTime = d3.timeParse('%Y-%m-%d');
+    const formatTooltipDate = d3.timeFormat('%m/%d');
+    const x = d3
+      .scaleBand()
+      .rangeRound([0, width])
+      .padding(0.35); // percentage
+    const y = d3.scaleLinear().range([height, 0]);
+
+    const svg = d3
+      .select('#' + this.props.id)
+      .append('svg')
+      .attr('width', width + this.margin.left + this.margin.right)
+      .attr('height', height + this.margin.top + this.margin.bottom)
+      .append('g')
+      .attr(
+        'transform',
+        'translate(' + this.margin.left + ',' + this.margin.top + ')'
+      );
+    //[fail, success]
+    let colors = d3.scaleOrdinal(['#5cb85c', '#d9534f']);
+    if (this.props.isAccessible) {
+      colors = d3.scaleOrdinal(['#92D400', '#A30000']);
     }
 
-    getApiUrl(name) {
-        return this.protocol + '://' + this.server + '/tower_analytics/' + name + '/';
+    const tooltip = new Tooltip({
+      svg: '#d3-chart-root > svg',
+      colors
+    });
+    const status = ['FAIL', 'RAN'];
+    let data = await d3.csv(
+      'https://gist.githubusercontent.com/kialam/52130f7e3292dad03a0c841f39a3b9d3/raw/ce1496e22c103cfd04314bac98c67eb8f7b8a7a1/sample.csv'
+    );
+    const half_length = Math.ceil(data.length / 2);
+
+    if (this.props.value === 'past 2 weeks') {
+      data = data.splice(0, half_length);
+    }
+    if (this.props.value === 'past week') {
+      data = data.splice(0, 7);
     }
 
-    shouldComponentUpdate() {
-        console.log('shouldComponentUpdate');
-        return false;
+    data.forEach(function(d) {
+      d.DATE = parseTime(d.DATE); // format date string into DateTime object
+      d.DATE = d.DATE; // format date string into DateTime object
+      d.RAN = +d.RAN;
+      d.FAIL = +d.FAIL;
+      d.TOTAL = +(+d.FAIL + +d.RAN);
+    });
+
+    // stack our data
+    const stack = d3
+      .stack()
+      .keys(status)
+      .order(d3.stackOrderNone)
+      .offset(d3.stackOffsetNone);
+
+    const layers = stack(data);
+    // Scale the range of the data
+    x.domain(layers[0].map(d => d.data.DATE));
+    y.domain([0, d3.max(layers[layers.length - 1], d => d[1])]).nice();
+    // Add the Y Axis
+    svg
+      .append('g')
+      .call(
+        d3
+          .axisLeft(y)
+          .ticks(8)
+          .tickSize(-width, 0, 0)
+      )
+      .selectAll('line')
+      .attr('stroke', '#d7d7d7');
+    // text label for the y axis
+    svg
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 0 - this.margin.left)
+      .attr('x', 0 - height / 2)
+      .attr('dy', '1em')
+      .style('text-anchor', 'middle')
+      .text('Jobs Across All Clusters');
+    // Add the X Axis
+    svg
+      .append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(
+        d3
+          .axisBottom(x)
+          .ticks(8)
+          .tickSize(-height)
+          .tickFormat(d3.timeFormat('%m/%d')) // "01/19"
+      )
+      .selectAll('line')
+      .attr('stroke', '#d7d7d7');
+    // text label for the x axis
+    svg
+      .append('text')
+      .attr(
+        'transform',
+        'translate(' + width / 2 + ' ,' + (height + this.margin.top + 20) + ')'
+      )
+      .style('text-anchor', 'middle')
+      .text('Date');
+
+    const layer = svg
+      .selectAll('layer')
+      .data(layers)
+      .enter()
+      .append('g')
+      .attr('class', 'layer')
+      .style('fill', (_d, i) => colors(i));
+    layer
+      .selectAll('rect')
+      .data(d => d)
+      .enter()
+      .append('rect')
+      .attr('x', d => x(d.data.DATE))
+      .attr('y', d => y(d[0]))
+      .attr('height', 0)
+      .attr('width', x.bandwidth() - 1)
+      .transition()
+      .duration(550)
+      .ease(d3.easeCubic)
+      .attr('y', d => y(d[1]))
+      .attr('height', d => y(d[0]) - y(d[1]));
+    layer
+      .selectAll('rect')
+      .on('mouseover', tooltip.handleMouseOver)
+      .on('mousemove', tooltip.handleMouseOver)
+      .on('mouseout', tooltip.handleMouseOut);
+
+    // Call the resize function whenever a resize event occurs
+    d3.select(window).on('resize', this.resize(this.init, 500));
+  }
+
+  async componentDidMount() {
+    this.init();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.value !== this.props.value) {
+      this.init();
     }
-
-    async componentDidMount() {
-        console.log('componentDidMount start');
-        console.log(ReactDOM.findDOMNode(this));
-        console.log(document.getElementById("#bar-chart-root"));
-        await this.drawChart();
-        console.log('componentDidMount end');
+    if (prevProps.isAccessible !== this.props.isAccessible) {
+      this.init();
     }
+  }
 
-    async drawChart() {
-        const url = this.getApiUrl('data');
-        const response = await fetch(url);
-        const data = [[12, 12, "01/01"], [50, 5, "01/02"], [6, 6, "01/03"], [6, 6, "01/04"], [9, 9, "01/05"], [10, 10, "01/06"]];
-        const totals = data.map(x => x[0] + x[1]);
-        console.log(data);
-        console.log(totals);
-
-        const y = d3.scaleLinear()
-        .domain([ 0, Math.max(...totals) ])
-        .range([ 0, 210 ]);
-
-        const chartBottom = this.props.height - 70;
-        const chartLeft = 70;
-
-        const parseTime = d3.timeParse('%m/%d');
-        const x2 = d3.scaleTime().range([ chartLeft + 40, (data.length - 1) * 70 + chartLeft + 40 ]);
-        const y2 = d3.scaleLinear().range([ 210, 0 ]);
-        x2.domain(d3.extent(data, function(d) { return parseTime(d[2]); }));
-        y2.domain([ 0, Math.max(...totals) ]);
-        y2.nice(5);
-
-        console.log("#" + this.props.id);
-        console.log(document.getElementById("#" + this.props.id));
-        console.log(d3.select("#" + this.props.id));
-        const svg = d3.select("#" + this.props.id)
-        .append('svg')
-        .attr('width', this.props.width)
-        .attr('height', this.props.height)
-        .style('margin-left', 100)
-        .style('background-color', 'white');
-        //.style('border-style', 'solid')
-        //.style('border-color', 'blue')
-        //.style('border-width', '5px');
-
-        // Add the y Axis
-        const svgYAxis = svg.append('g');
-
-        svgYAxis.attr('transform', 'translate(' + chartLeft + ',' + (chartBottom - 210) + ')')
-        .call(d3.axisRight(y2)
-        .ticks(5)
-        .tickSize(this.props.width - chartLeft - 50))
-        .selectAll('.domain').attr('stroke', '#d7d7d7');
-        svgYAxis.selectAll('.tick text').attr('x', -5).attr('dy', 4).attr('fill', '#393f44').attr('text-anchor', 'end');
-        svgYAxis.selectAll('.tick line').attr('stroke', '#d7d7d7');
-
-        const svgChart = svg.append('g');
-
-        const columns = svgChart.selectAll('rect')
-        .data(data)
-        .enter()
-        .append('g')
-        .attr('data-failures', (d) => d[1])
-        .attr('data-passes', (d) => d[0])
-        .attr('data-total', (d) => d[0] + d[1])
-        .on('mouseover', handleMouseOver)
-        .on('mousemove', handleMouseOver)
-        .on('mouseout', handleMouseOut);
-
-        columns.append('rect')
-        .attr('x', (d, i) => i * 70 + chartLeft + 25)
-        .attr('y', (d) => chartBottom - y(d[0]))
-        .attr('width', 30)
-        .attr('height', (d) => y(d[0]))
-        .attr('fill', '#d9534f');
-
-        columns.append('rect')
-        .attr('x', (d, i) => i * 70 + chartLeft + 25)
-        .attr('y', (d) => chartBottom - y(d[1]) - y(d[0]))
-        .attr('width', 30)
-        .attr('height', (d) => y(d[1]) - 1)
-        .attr('fill', '#5cb85c');
-
-        // Add the x Axis
-        const svgXAxis = svg.append('g');
-        svgXAxis.attr('transform', 'translate(0,' + chartBottom + ')')
-        .call(d3.axisBottom(x2)
-        .ticks(data.length)
-        .tickFormat(d3.timeFormat('%m/%d')));
-        svgXAxis.selectAll('.domain').attr('stroke', '#d7d7d7');
-        svgXAxis.selectAll('text').attr('fill', '#393f44');
-        svgXAxis.selectAll('line').attr('stroke', '#d7d7d7');
-
-        // text label for the y axis
-        svg.append('text')
-        .attr('transform', 'translate(30, ' + this.props.height / 2 + ') rotate(-90)')
-        .style('text-anchor', 'middle')
-        .text('Jobs Across All Clusters')
-        .attr('fill', '#393f44');
-
-        // text label for the x axis
-        svg.append('text')
-        .attr('transform', 'translate(' + this.props.width / 2 + ', ' + (this.props.height - 30) + ')')
-        .style('text-anchor', 'middle')
-        .text('Time: Day')
-        .attr('fill', '#393f44');
-
-        // tooltip
-
-        const tooltip = svg.append('g');
-        tooltip.attr('id', 'svg-chart-tooltip');
-        tooltip.style('opacity', 0);
-        tooltip.style('pointer-events', 'none');
-        tooltip.attr('transform', 'translate(100, 100)');
-        tooltip.append('rect')
-        .attr('transform', 'translate(10, -10) rotate(45)')
-        .attr('x', 0)
-        .attr('y', 0)
-        .attr('height', 20)
-        .attr('width', 20)
-        .attr('fill', '#393f44');
-        tooltip.append('rect')
-        .attr('x', 10)
-        .attr('y', -41)
-        .attr('rx', 2)
-        .attr('height', 82)
-        .attr('width', 135)
-        .attr('fill', '#393f44');
-        tooltip.append('circle')
-        .attr('cx', 26)
-        .attr('cy', 0)
-        .attr('r', 7)
-        .attr('stroke', 'white')
-        .attr('fill', 'green');
-        tooltip.append('circle')
-        .attr('cx', 26)
-        .attr('cy', 26)
-        .attr('r', 7)
-        .attr('stroke', 'white')
-        .attr('fill', 'red');
-        tooltip.append('text')
-        .attr('x', 43)
-        .attr('y', 4)
-        .attr('font-size', 12)
-        .attr('fill', 'white')
-        .text('Successful');
-        tooltip.append('text')
-        .attr('x', 43)
-        .attr('y', 28)
-        .attr('font-size', 12)
-        .attr('fill', 'white')
-        .text('Failed');
-        tooltip.append('text')
-        .attr('fill', 'white')
-        .attr('stroke', 'white')
-        .attr('x', 24)
-        .attr('y', 30)
-        .attr('font-size', 12)
-        .text('!');
-        const jobs = tooltip.append('text')
-        .attr('fill', 'white')
-        .attr('x', 137)
-        .attr('y', -21)
-        .attr('font-size', 12)
-        .attr('text-anchor', 'end')
-        .text('No Jobs');
-        const failed = tooltip.append('text')
-        .attr('fill', 'white')
-        .attr('font-size', 12)
-        .attr('x', 122)
-        .attr('y', 4)
-        .text('0');
-        const successful = tooltip.append('text')
-        .attr('fill', 'white')
-        .attr('font-size', 12)
-        .attr('x', 122)
-        .attr('y', 28)
-        .text('0');
-        const date = tooltip.append('text')
-        .attr('fill', 'white')
-        .attr('stroke', 'white')
-        .attr('x', 20)
-        .attr('y', -21)
-        .attr('font-size', 12)
-        .text('Never');
-
-        function handleMouseOver(d) {
-            const coordinates = d3.mouse(this);
-            const x = coordinates[0] + 5;
-            const y = coordinates[1] - 5;
-
-            date.text(d[2]);
-            jobs.text('' + this.dataset.total + ' Jobs');
-            failed.text('' + this.dataset.failures);
-            successful.text('' + this.dataset.passes);
-            tooltip.attr('transform', 'translate(' + x + ',' + y + ')');
-            tooltip.style('opacity', 1);
-            tooltip.interrupt();
-        }
-
-        function handleMouseOut() {
-            tooltip.transition()
-            .delay(15)
-            .style('opacity', 0)
-            .style('pointer-events', 'none');
-        }
-        console.log('done');
-
-    }
-
-    render () {
-        return <div id={ this.props.id }></div>;
-    }
+  render() {
+    return <div id={this.props.id} />;
+  }
 }
-BarChart.propTypes = {
-    height: PropTypes.number,
-    width: PropTypes.number,
-    id: PropTypes.string
-};
 
 export default BarChart;
